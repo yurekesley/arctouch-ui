46c76d0271492bc536fa89abd5bc0e449a108f33
MDIAS\ter01282 <yurekesley@gmail.com>
2019-09-10 13:20:48 -0300

    melhorando visualizacao detalhe

--- src/app/pages/arctouch/detalhe-filme/detalhe-filme.module.ts
+++ src/app/pages/arctouch/detalhe-filme/detalhe-filme.module.ts
@@ -1,9 +1,13 @@
+import { MDBContainerModule, MDBGridModule, MDBI18NModule } from 'mdias-componentes';
 import { DetalheModule } from './../../../core/components/detalhe/detalhe.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DetalheFilmeRoutingModule } from './detalhe-filme-routing.module';
 import { DetalheFilmeComponent } from './detalhe-filme.component';
+import { SMTTituloModule } from 'src/app/core/components/smt-titulo/smt-titulo.module';
+import { SMTChaveValorModule } from 'src/app/core/pipes/chave-valor/chave-valor.module';
+import { SMTContainerModule } from 'src/app/core/components/smt-container/smt-container.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +16,12 @@ import { DetalheFilmeComponent } from './detalhe-filme.component';
   imports: [
     CommonModule
     , DetalheModule
+    , MDBContainerModule
+    , MDBI18NModule
+    , MDBGridModule
+    , SMTTituloModule
+    , SMTChaveValorModule
+    , SMTContainerModule
     , DetalheFilmeRoutingModule
   ]
 })
